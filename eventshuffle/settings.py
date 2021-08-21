605a2efb7bd11d6b4e8a3a2daeba6e0a70ee2fbd
Pekka Piispanen <pekka@vilkas.fi>
2021-08-21 10:19:17 +0300

    Added event and rest framework to installed apps

--- eventshuffle/settings.py
+++ eventshuffle/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party
+    'rest_framework',
+
+    # Our own apps
+    'eventshuffle.event',
 ]
 
 MIDDLEWARE = [
