a071e7b95fbc2b1c51403589cff53d964d8bf98f
Pekka Piispanen <pekka@vilkas.fi>
2021-08-22 10:25:39 +0300

    Added new models for event date and the voter

--- eventshuffle/event/models.py
+++ eventshuffle/event/models.py
@@ -22,3 +22,64 @@ class Event(models.Model):
         _("Event name"),
         max_length=255
     )
+
+    def __str__(self):
+        return f"{self.name} ({self.id})"
+
+    def get_dates(self):
+        """Gets event related dates
+
+        Returns:
+            list: All the suitable dates for the event
+        """
+        return [ed.date for ed in EventDate.objects.filter(event=self)]
+
+
+class EventDateVoter(models.Model):
+    """
+    The class for event voters
+    """
+
+    id = models.UUIDField(
+        _("Unique id for the event voter"),
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
+    name = models.CharField(
+        _("Event voter name"),
+        max_length=255
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.id})"
+
+
+class EventDate(models.Model):
+    """
+    The connecting model for events and dates
+    """
+    id = models.UUIDField(
+        _("Unique id for the event / date connection"),
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE
+    )
+
+    date = models.DateField(
+        _("Suitable date for the event"),
+    )
+
+    voters = models.ManyToManyField(
+        EventDateVoter,
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.event.name} - {self.date} ({self.id})"
--- eventshuffle/event/serializers.py
+++ eventshuffle/event/serializers.py
@@ -6,6 +6,14 @@ from .models import Event
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
+    dates = serializers.SerializerMethodField()
+
     class Meta:
         model = Event
-        fields = ["id", "url", "name"]
+        fields = ["id", "url", "name", "dates"]
+
+    def get_dates(self, obj):
+        """
+        Get the dates for the event.
+        """
+        return obj.get_dates()
