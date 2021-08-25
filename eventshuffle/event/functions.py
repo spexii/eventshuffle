1002f3c218898816d90bdd9056f08d350abf059e
Pekka Piispanen <pekka@vilkas.fi>
2021-08-25 10:45:19 +0300

    Modified the date validation to check also dates for event

--- eventshuffle/event/functions.py
+++ eventshuffle/event/functions.py
@@ -1,18 +1,28 @@
 # Standard library imports
 from datetime import datetime
 
-def validate_dates(dates):
-    """Checks the format of given dates (YYYY-MM-DD) and that
-    dates are not in the past.
+def validate_dates(dates, event=None):
+    """Checks the format of given dates (YYYY-MM-DD) and if the dates
+    are valid.
+
+    If event is going to be created (event argument is not given), there's
+    a check that the dates are not in the past.
+    
+    If an existing event is given, check if the given dates are dates
+    for the event.
 
     Args:
         dates (list): List of date strings
+        event (Event, optional): An existing event. Defaults to None.
 
     Returns:
         str: An error message or empty string
     """
+    # Get event dates for later check, if event is given
+    event_dates = event.get_dates() if event else []
+
     # Check if dates are given in a list. If not, skip rest of the checking
-    if not dates or type(dates) is not list or len(dates) == 0:
+    if type(dates) is not list or len(dates) == 0:
         return "A list of dates must be given!"
 
     for date_str in dates:
@@ -22,8 +32,13 @@ def validate_dates(dates):
         except:
             return f"Invalid date: {date_str}"
 
-        # Check that date is not in the past
-        if date_obj < datetime.now().date():
-            return f"Date can't be in the past: {date_str}"
+        # Check that date is not in the past, if event is not given
+        if not event:
+            if date_obj < datetime.now().date():
+                return f"Date can't be in the past: {date_str}"
+        # Otherwise, check if the date if one of the event dates
+        else:
+            if not date_obj in event_dates:
+                return f"Date isn't one of the event dates: {date_str}"
 
     return ""
